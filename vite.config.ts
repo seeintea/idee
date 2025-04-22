c31cd845ecdcb654fd0c286b59e4444d2ad98263
leviegu <gu.lw@foxmail.com>
2025-04-22 21:22:05 +0800

    chore(deps): use react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import babel from "vite-plugin-babel";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
 });
