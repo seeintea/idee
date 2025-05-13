da7714089afb6b653312952821db46129813c501
leviegu <leviegu@gmail.com>
2025-05-13 23:04:59 +0800

    style: use `Graphite` theme

--- app/layout/app-layout/sidebar-item.tsx
+++ app/layout/app-layout/sidebar-item.tsx
@@ -4,6 +4,7 @@ import { RiJavascriptFill, RiReactjsLine } from "react-icons/ri";
 import { type MenusItem } from "~/utils/routes";
 import Space from "~/components/space";
 import { Button } from "~/components/ui/button";
+import { cn } from "~/utils/tailwind";
 
 const icons = {
   JAVASCRIPT: <RiJavascriptFill size={20} />,
@@ -21,9 +22,9 @@ export default function SidebarItem(props: MenusItem) {
       {props.children.map((route) => (
         <Button
           key={route.path}
-          variant={pathname === route.path ? "secondary" : "ghost"}
+          variant={pathname === route.path ? "outline" : "link"}
           asChild
-          className={"w-full justify-start my-1"}
+          className={cn("w-full justify-start my-1", pathname === route.path ? "bg-background!" : "")}
         >
           <a href={route.path}>{route.name}</a>
         </Button>
