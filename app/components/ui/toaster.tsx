39d39a6425ce25ed20ba8ab1211dd0f7c793553c
leviegu <leviegu@gmail.com>
2025-05-31 14:21:21 +0800

    chore: update deps

--- app/components/ui/toaster.tsx
+++ app/components/ui/toaster.tsx
@@ -2,15 +2,12 @@
 // Copyright (c) 2023 shadcn
 // https://github.com/shadcn-ui/ui/blob/main/apps/www/registry/new-york/ui/sonner.tsx
 
-import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={"light"}
       className="toaster group"
       toastOptions={{
         style: {
